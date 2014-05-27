56b72cb774163b14842783324dce9689121822a5
TheDr1ver <driver.nick@gmail.com>
2014-05-27 16:07:21 -0400

    Update config.php

--- func/config.php
+++ func/config.php
@@ -32,6 +32,29 @@ $remver='4';
 #REMnux v4
 $tridLoc = '/usr/local/lib/triddefs.trd';
 
+#################################
+##### ThreatAnalyzer Integration
+#################################
+
+$threatAnalyzerPlugin=False;	#False by default - set TRUE to upload & run files with ThreatAnalyzer using WIPSTER
+
+#Vars
+$threatAPI = '';	#ThreatAnalyzer API grabbed from ThreatAnalyzer Admin console
+$threatBase = '';	#IP address of your ThreatAnalyzer Server (ex. 192.168.1.100)
+$threatPage = 'http://'.$threatBase.'/api/v1';	#Leave this alone
+$threatArgs = '';	#Leave blank unless specifically adding additional arguments to the API call (ex. 'md5=[md5]&setting=whatever')
+
+#Submission Variables
+
+$taSubPriority = 'high';	# Submission priority (ex. high, medium, low)
+$taSubSandbox = '';	#MAC address of Sandbox to submit file to (ex: 00:11:22:33:44:55)
+$taSubReanalyze = 'false';	# Reanalyze files that have previously been submitted
+
+#UnComment the following lines if you want to run a custom action:
+#$taSubCustomName = 'ActionAfterAnalysis';	#Custom Action name
+#$taSubCustomVal = 'revert';	#Custom Action value
+	#Additional Custom actions can be added manually in $postArray on md5page.php and accept-file.php
+
 ##################
 ##### ANUBIS #####
 ##################
@@ -49,7 +72,6 @@ $apis=array();
 # Request API Key at https://www.mywot.com/en/signup?destination=profile/api
 
 $apis['wot']='Your MyWOT API';
-
 ######################
 ##### VirusTotal #####
 ######################
@@ -69,23 +91,20 @@ $apis['goog']='Your Google SafeBrowsing API';
 #######################################
 ##### Google Custom Search Engine #####
 #######################################
-# Set up your custom search at https://www.google.com/cse/all
+#Set up your custom search at https://www.google.com/cse/all
 
-# CSE API Key
+#CSE API Key
 $gcseKey = 'Your Custom Search Engine API';
 
-# CSE Signature
+#CSE Signature
 $gcseSig = 'Your Custom Search Engine Signature Key';
 
-# Custom Search Engine ID
-$gcseCx = 'Your Custom Search Engine ID'; #Tied to a dummy acct
+#Custom Search Engine ID
+$gcseCx = 'Your Custom Search Engine ID'; 
 
-	########################
-	##### AutoPastebin #####
-	########################
-
-	$gcseQuery = '%22TheDr1ver%22';	#Whatever default search term you want to search pastebin for
-	$autopbUA = 'AutoPastebinSearch';	#Set to whatever useragent you want to use
+	##### AutoPastebin
+	$gcseQuery = 'SearchTerm';	#Whatever default search term you want to search pastebin for automatically
+	$autopbUA = 'AutoPastebinSearch';	#Set to whatever useragent you want to use for the pastebin searching
 
 ##########################
 ##### Twitter Widget #####
@@ -97,7 +116,5 @@ $twitterToken = 'Twitter Access Token';
 $twitterQuery = '#0Day -RT filter:links';	#Searches for #0Day, ignoring Re-Tweets, and showing only posts with links
 $twitterConSec = 'Twitter Consumer Secret';
 $twitterOAuthSec = 'Twitter Access Token Secret';
-
-
 
 ?>
