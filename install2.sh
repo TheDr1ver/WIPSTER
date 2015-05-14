9967f560961ffe82ee85e87b079e8f82415117a2
Nick Driver <driver.nick@gmail.com>
2015-05-14 13:22:44 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -14,41 +14,47 @@ then
 	exit #E_NOTROOT
 fi
 
-echo "Installing dependencies..."
-apt-get update
-apt-get install php5-fpm php5-mcrypt php5-curl php5-sqlite -y
+if [ -f /var/www/admin/admin.db ];
+then
+	echo "WIPSTER is already installed."
+else
+	echo "Installing dependencies..."
+	apt-get update
+	apt-get install php5-fpm php5-mcrypt php5-curl php5-sqlite -y
+	
+	#Modify nginx and MASTIFFconfigs
+	echo "Modifying nginx and mastiff configs..."
+	sed -i -e 's/index index\.html.*/index index.php index.html index.htm/g' /etc/nginx/sites-enabled/default
+	
+	sed -i -e '/#location.*php\$.*/s/#//g' /etc/nginx/sites-enabled/default
+	sed -i -e '/location.*php\$.*/a \\ \t\ttry_files \$uri =404;' /etc/nginx/sites-enabled/default
+	sed -i -e '/fastcgi_split_path_info.*php.*/s/#//g' /etc/nginx/sites-enabled/default
+	sed -i -e '/fastcgi_pass.*php.*/s/#//g' /etc/nginx/sites-enabled/default
+	sed -i -e '/fastcgi_index.*php.*/s/#//g' /etc/nginx/sites-enabled/default
+	sed -i -e '/include fastcgi_params.*/s/#//g' /etc/nginx/sites-enabled/default
+	sed -i -e '/include fastcgi_params.*/a \\ \n\t}' /etc/nginx/sites-enabled/default
+	
+	sed -i -e 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/fpm/php.ini
+	sed -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php5/fpm/php.ini
+	
+	sed -i -e 's/^log_dir.*/log_dir = \/var\/www\/mastiff/g' /etc/mastiff/mastiff.conf
+	
+	#Downloading and installing WIPSTER
+	
+	mkdir /opt/remnux-wipster/
+	cd /opt/remnux-wipster/
+	git clone -b remnux-v6 --single-branch https://github.com/TheDr1ver/WIPSTER.git
+	cp -rf /opt/remnux-wipster/WIPSTER/.* /var/www/
+	chown -R www-data:www-data /var/www/
+	service nginx stop
+	service nginx start
+	service php5-fpm restart
+	
+	
+	#Launching config page
+	echo "WIPSTER installed successfully! To configure, visit http://127.0.0.1/admin.php"
+	
+	#Remove install Dir
+	rm -r /opt/remnux-wipster/
+fi
 
-#Modify nginx and MASTIFFconfigs
-echo "Modifying nginx and mastiff configs..."
-sed -i -e 's/index index\.html.*/index index.php index.html index.htm/g' /etc/nginx/sites-enabled/default
-
-sed -i -e '/#location.*php\$.*/s/#//g' /etc/nginx/sites-enabled/default
-sed -i -e '/location.*php\$.*/a \\ \t\ttry_files \$uri =404;' /etc/nginx/sites-enabled/default
-sed -i -e '/fastcgi_split_path_info.*php.*/s/#//g' /etc/nginx/sites-enabled/default
-sed -i -e '/fastcgi_pass.*php.*/s/#//g' /etc/nginx/sites-enabled/default
-sed -i -e '/fastcgi_index.*php.*/s/#//g' /etc/nginx/sites-enabled/default
-sed -i -e '/include fastcgi_params.*/s/#//g' /etc/nginx/sites-enabled/default
-sed -i -e '/include fastcgi_params.*/a \\ \n\t}' /etc/nginx/sites-enabled/default
-
-sed -i -e 's/short_open_tag = Off/short_open_tag = On/g' /etc/php5/fpm/php.ini
-sed -i -e 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php5/fpm/php.ini
-
-sed -i -e 's/^log_dir.*/log_dir = \/var\/www\/mastiff/g' /etc/mastiff/mastiff.conf
-
-#Downloading and installing WIPSTER
-
-mkdir /opt/remnux-wipster/
-cd /opt/remnux-wipster/
-git clone -b remnux-v6 --single-branch https://github.com/TheDr1ver/WIPSTER.git
-cp -rf /opt/remnux-wipster/WIPSTER/.* /var/www/
-chown -R www-data:www-data /var/www/
-service nginx stop
-service nginx start
-service php5-fpm restart
-
-
-#Launching config page
-echo "WIPSTER installed successfully! To configure, visit http://127.0.0.1/admin.php"
-
-#Remove install Dir
-rm -r /opt/remnux-wipster/
